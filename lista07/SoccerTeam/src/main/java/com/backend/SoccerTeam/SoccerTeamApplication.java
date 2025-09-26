454bfea4df835bf2cb3d55a92d60301098c7f445
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-09-25 21:37:29 -0300

    :big: fix(lista07): recriar pasta da atividade de timde de jogadores

---
+++ lista07/SoccerTeam/src/main/java/com/backend/SoccerTeam/SoccerTeamApplication.java
@@ -0,0 +1,13 @@
+package com.backend.SoccerTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerTeamApplication.class, args);
+	}
+
+}
---
+++ lista07/SoccerTeam/src/test/java/com/backend/SoccerTeam/SoccerTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.SoccerTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
